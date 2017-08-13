153bd824b66d1eb2d0a08cd82036b4af6980e51c
Roberto Mauro <erremauro@icloud.com>
2017-08-13 17:46:08 +0200

    Sanitize string exit code in run-command.js

--- packages/rear-core/run-command.js
+++ packages/rear-core/run-command.js
@@ -6,20 +6,34 @@ module.exports = runCommand;
 //////////////////////////////
 
 function runCommand (cmd, args = [], stdio = 'inherit') {
+
   return new Promise((resolve, reject) => {
     const proc = spawn(cmd, args, { stdio });
+    const completeCommand = `${cmd} ${args.join(' ')}`.trim();
+
     proc.on('error', err => {
-      reject(new CommandFailure({
-        command: `${cmd} ${args.join(' ')}`
-      }));
+      reject(new CommandFailure(completeCommand));
     });
+
     proc.on('close', code => {
+      code = sanitizeCode(code);
+
       if (code !== 0) {
-        reject(new CommandFailure({
-          command: `${cmd} ${args.join(' ')}`
-        }));
+        reject(new CommandFailure(completeCommand, code));
+        return;
       }
       resolve();
     });
   });
+}
+
+function sanitizeCode (code) {
+  if (typeof code === 'string') {
+    try {
+      code = parseInt(code, 10);
+    } catch (err) {
+      // Ignore errors
+    }
+  }
+  return code;
 }
